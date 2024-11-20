b25e2c25f81f496c35084d258f199be2bab22124
Pratik Kumar <pratikumar56@gmail.com>
2024-11-20 15:12:58 +0530

    added a controller for contactus page

--- routes/contactus.js
+++ routes/contactus.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 
+const contactController = require('../controllers/contact');
+
 const routes = express.Router();
 
-routes.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'contactus.html'));
-});
+routes.get('/', contactController.getContact);
 
 module.exports = routes;
